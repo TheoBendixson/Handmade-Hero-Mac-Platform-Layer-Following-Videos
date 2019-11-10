b46e23362fd803df4548d66940ab8d0a93d41085
Theodore Bendixson <tedbendixson@me.com>
2019-11-09 17:25:14 -0700

    Day 17 Platform Independent Memory

---
+++ Day017/handmade/code/build.sh
@@ -0,0 +1,14 @@
+echo Building Handmade Hero
+
+INCLUDED_FRAMEWORKS="-framework AppKit
+                     -framework IOKit
+                     -framework AudioToolbox"
+
+mkdir ../../build
+pushd ../../build
+rm -rf handmade.app
+mkdir -p handmade.app
+clang -g "-DHANDMADE_INTERNAL=1" $INCLUDED_FRAMEWORKS -o handmade ../handmade/code/osx_main.mm
+cp handmade handmade.app/handmade
+cp "../handmade/resources/Info.plist" handmade.app/Info.plist
+popd
